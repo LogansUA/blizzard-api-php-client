badecc82305f007c8e9aed87cecb21bfb260f9b1
LogansUA <logansoleg@gmail.com>
2015-12-28 02:13:52 +0200

    Update examples

--- examples/GameDataExample.php
+++ examples/GameDataExample.php
@@ -7,7 +7,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $client = new \BlizzardApi\BlizzardClient('apiKey');
 
 // Create a new GameData service with configured Blizzard client
-$gameData = new \BlizzardApi\Service\GameData($client);
+$gameData = new \BlizzardApi\Service\GameData($client->setAccessToken('accessToken'));
 
 // Use API method for getting specific data
 $response = $gameData->getEraLeaderboard(1, 'rift-barbarian');
