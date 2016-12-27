5de89c7e09ab811fa7e8f02c218a5f5b8909d3bc
duronrulez <duronrulez@gmail.com>
2016-12-27 14:22:59 +0200

    Update Examples
    Updating the examples to reflect the new way of using access tokens.

--- examples/GameDataExample.php
+++ examples/GameDataExample.php
@@ -4,10 +4,10 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 // Create a new Blizzard client with Blizzard API key
-$client = new \BlizzardApi\BlizzardClient('apiKey');
+$client = new \BlizzardApi\BlizzardClient('apiKey', 'apiSecret');
 
 // Create a new GameData service with configured Blizzard client
-$gameData = new \BlizzardApi\Service\GameData($client->setAccessToken('accessToken'));
+$gameData = new \BlizzardApi\Service\GameData($client);
 
 // Use API method for getting specific data
 $response = $gameData->getEraLeaderboard(1, 'rift-barbarian');
