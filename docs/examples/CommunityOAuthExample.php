3b10f3355f5809ba898dcc885f1330e96ffa8965
Oleg Kachinsky <Logansoleg@gmail.com>
2017-04-03 12:58:17 +0300

    Update CommunityOAuthExample.php

--- docs/examples/CommunityOAuthExample.php
+++ docs/examples/CommunityOAuthExample.php
@@ -7,7 +7,7 @@ require_once __DIR__.'/../../vendor/autoload.php';
 $client = new \BlizzardApi\BlizzardClient('apiKey', 'apiSecret');
 
 // Create a new Diablo service with configured Blizzard client
-$diablo = new \BlizzardApi\Service\Starcraft($client);
+$diablo = new \BlizzardApi\Service\Diablo($client);
 
 // Use API method for getting specific data
 $response = $diablo->getProfileUser();
